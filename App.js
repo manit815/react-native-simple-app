addecc6ad93cf61eb4e94450f6913a1470b1d37f
Manit Kant <manit_kant@yahoo.com>
2021-05-02 13:07:51 +0530

    inlcuding fetch along with some styling

--- App.js
+++ App.js
@@ -1,21 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import { StyleSheet, Text, FlatList, Pressable, Alert } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
 export default function App() {
+  const [countriesData, setCountriesData] = useState([])
+  function fetchCountriesData() {
+    fetch('https://restcountries.eu/rest/v2/region/africa?fields=name;capital')
+      .then((response) => response.json())
+      .then((json) => setCountriesData(json))
+      .catch((error) => console.error(error))
+  }
+  useEffect(()=> {
+    fetchCountriesData();
+  })
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <StatusBar style='light'/>
+      <FlatList
+        data={countriesData}
+        contentContainerStyle={styles.container}
+        keyExtractor={item => item.name}
+        renderItem={({item})=> <Text onPress={() => {Alert.alert(`The Capital of ${item.name} is ${item.capital}`)}} style={styles.text}>{item.name}</Text>}
+      />
+    </>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 30,
+    backgroundColor: '#483D8B'
+  },
+  text: {
+    fontSize: 18,
+    margin: 5,
+    color: '#fff'
   },
 });
